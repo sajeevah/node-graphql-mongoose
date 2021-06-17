507e4c32d0791155af0f6d484e9f8bc96b5170d9
sajeevah <sa.hasith@gmail.com>
2021-06-17 20:42:35 +0530

    add mongoose

---
+++ src/database/mongoose.ts
@@ -0,0 +1,37 @@
+import { connect, disconnect } from "mongoose";
+
+
+const uri = '';
+
+export const dbConnection = (() : Promise<void> => {
+    return new Promise((resolve, reject) => {
+        connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+            if (err) {
+              console.log(err.message);
+              console.log(err);
+              reject();
+            }
+            else {
+              console.log('Connected to MongoDb');
+              resolve();
+            }
+        });
+    })
+});
+
+
+export const dbDisconnect = (() : Promise<void> => {
+    return new Promise((resolve, reject) => {
+        disconnect((err) => {
+            if (err) {
+              console.log(err.message);
+              console.log(err);
+              reject();
+            }
+            else {
+              console.log('Disconnected to MongoDb');
+              resolve();
+            }
+        });
+    })
+});
