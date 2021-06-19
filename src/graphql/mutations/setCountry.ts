be7fd8645c5804fc20b39911a8d1062b0868197e
sajeevah <sa.hasith@gmail.com>
2021-06-19 22:31:03 +0530

    setCountry mutation

---
+++ src/graphql/mutations/setCountry.ts
@@ -0,0 +1,12 @@
+import { Country, ICountry } from "../../models/country.model";
+
+export const setCountry = async (country: ICountry): Promise<ICountry> =>  {
+    try {
+        const countryModel = new Country(country);
+        const savedCountry: ICountry = await countryModel.save();
+        return savedCountry;
+    } catch (error) {
+        console.error('error in set Country : ', error);
+        throw new Error(error);
+    }
+}
