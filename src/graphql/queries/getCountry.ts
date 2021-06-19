66b59d94ca993631411f1e91e6c216dda02de10f
sajeevah <sa.hasith@gmail.com>
2021-06-19 23:28:43 +0530

    country and location populate data

--- src/graphql/queries/getCountry.ts
+++ src/graphql/queries/getCountry.ts
@@ -2,7 +2,7 @@ import { Country, ICountry } from '../../models/country.model';
 
 export const getCountry = async (): Promise<ICountry[]> => {
     try {
-        const countries: ICountry[] = await Country.find({ archived: false });
+        const countries: ICountry[] = await Country.find({ archived: false }).populate('locations');
         return countries;
     } catch (error) {
         console.error('error in getCountry : ', error);
--- src/graphql/queries/getLocation.ts
+++ src/graphql/queries/getLocation.ts
@@ -2,7 +2,7 @@ import { ILocation, Location } from '../../models/location.model';
 
 export const getLocation = async (): Promise<ILocation[]> => {
     try {
-        const locationes: ILocation[] = await Location.find({ archived: false });
+        const locationes: ILocation[] = await Location.find({ archived: false }).populate('country');
         return locationes;
     } catch (error) {
         console.error('error in getLocation : ', error);
