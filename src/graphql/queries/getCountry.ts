1f5d537a89908b5acc5314c0efcca921f8987434
sajeevah <sa.hasith@gmail.com>
2021-06-19 23:19:25 +0530

    getCountry

---
+++ src/graphql/queries/getCountry.ts
@@ -0,0 +1,11 @@
+import { Country, ICountry } from '../../models/country.model';
+
+export const getCountry = async (): Promise<ICountry[]> => {
+    try {
+        const countries: ICountry[] = await Country.find({ archived: false });
+        return countries;
+    } catch (error) {
+        console.error('error in getCountry : ', error);
+        throw new Error(error);
+    }
+}
