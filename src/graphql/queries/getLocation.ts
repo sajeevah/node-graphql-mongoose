098e64d3ae0854a10c0377f97a374b89f813c8b8
sajeevah <sa.hasith@gmail.com>
2021-06-19 23:21:01 +0530

    getLocation

---
+++ src/graphql/queries/getLocation.ts
@@ -0,0 +1,11 @@
+import { ILocation, Location } from '../../models/location.model';
+
+export const getLocation = async (): Promise<ILocation[]> => {
+    try {
+        const locationes: ILocation[] = await Location.find({ archived: false });
+        return locationes;
+    } catch (error) {
+        console.error('error in getLocation : ', error);
+        throw new Error(error);
+    }
+}
