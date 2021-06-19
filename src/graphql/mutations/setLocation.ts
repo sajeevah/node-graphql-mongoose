3b8f8a2cdb2fa113e6bdab995d5c01163719aa92
sajeevah <sa.hasith@gmail.com>
2021-06-19 22:43:07 +0530

    set location

---
+++ src/graphql/mutations/setLocation.ts
@@ -0,0 +1,17 @@
+import { Country } from "../../models/country.model";
+import { ILocation, Location } from "../../models/location.model";
+
+
+export const setLocation = async (location: ILocation): Promise<ILocation> => {
+    try {
+        const locationModel = new Location(location);
+        const savedLocation = await locationModel.save();
+        if (savedLocation) {
+            await Country.findByIdAndUpdate(location.country, { $push: savedLocation?._id }, { new: true });
+        }
+        return savedLocation
+    } catch (error) {
+        console.error('error in set location : ', error);
+        throw new Error(error);
+    }
+}
